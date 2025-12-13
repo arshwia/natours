b64c0f0d867e9083972f9e1cfb59057f30bc9262
arshwia <arshia.hy.coding@gmail.com>
2025-12-13 18:06:32 +0330

    ta in ga tisti bod

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello from the server side!💕😁',
+        app: 'Natours',
+    });
+});
+
+app.post('/', (req, res) => {
+    res.status(200).send('you can post to this URL');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`app runing on port : ${port}`);
+});
