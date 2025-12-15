1b4428b311912a469d7c40343de89afa1b8aaebc
arshwia <arshia.hy.coding@gmail.com>
2025-12-15 16:15:12 +0330

    in 'app.js' 1. add a get metod for get one tour bu id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const express = require('express');
+const { toUSVString } = require('util');
 
 const app = express();
 
@@ -15,6 +16,26 @@ app.get('/api/v1/tours', (req, res) => {
         results: tours.length,
         data: {
             tours: tours,
+        },
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+
+    // if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'this tour is not found💔',
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours: tour,
         },
     });
 });
