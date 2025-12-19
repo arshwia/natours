cb8aa87e3aa86b7e221db9ccce00d64e51d9606f
arshwia <arshia.hy.coding@gmail.com>
2025-12-20 02:01:26 +0330

    in '/controllers' 1. فانکشن های که روت ها مون رو کنترل میکردن ریخیتم تو این

---
+++ controllers/userControllers.js
@@ -0,0 +1,42 @@
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'this rout is not yet difned!',
+    });
+};
+
+const creatUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'this rout is not yet difned!',
+    });
+};
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'this rout is not yet difned!',
+    });
+};
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'this rout is not yet difned!',
+    });
+};
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'this rout is not yet difned!',
+    });
+};
+
+module.exports = {
+    getAllUsers,
+    creatUser,
+    getUser,
+    updateUser,
+    deleteUser,
+};
