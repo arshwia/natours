3ab57f2e3b8308501289b203e5ed5cbb5cacdd20
arshwia <arshia.hy.coding@gmail.com>
2026-02-15 18:02:30 +0330

    in'server.js' 1. add a line 7 & 8

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+// for conncted db shell : mongosh "mongodb+srv://natours.sbib7js.mongodb.net/" --apiVersion 1 --username admin
+// for start DATABASE : sudo systemctl start mongod
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+    process.env.DATABASE_PASSWORD,
 );
-
-mongoose.connect(DB).then((con) => console.log('db connection successful!'));
+mongoose.connect(DB).then(() => console.log('db connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
